a1eef7df884dbd911cdbbda1e040d96a91b0127a
Cristian David Moraleda <cm@qubikdigital.com>
2023-03-30 23:25:19 -0300

    Agregados comentarios ejercicio 10

--- TP2/EJ10/main.c
+++ TP2/EJ10/main.c
@@ -10,6 +10,7 @@ int main() {
     inicpila(&diferentes);
     inicpila(&pilaUnion);
 
+    ///Este for carga la pila "pilaA" del 1 al 3, y la pila "pilaB" del 2 al 7
     for (int i = 1; i < 8; i++) {
         if (i < 4) {
             apilar(&pilaA, i);
@@ -19,29 +20,41 @@ int main() {
         }
     }
 
+    ///En este ciclo while copio "pilaA" a "auxiliarA"
     while (!pilavacia(&pilaA)){
         apilar(&auxiliarA,tope(&pilaA));
         pasar(&pilaUnion,&pilaA);
     }
+    ///pasarTodo(&destino,&origen) es una funcion mia, que pasa el contenido de "origen" a "destino"
     pasarTodo(&pilaA,&pilaUnion);
 
+    ///En este ciclo while copio "pilaB" a "auxiliarB"
     while (!pilavacia(&pilaB)){
         apilar(&auxiliarB,tope(&pilaB));
         pasar(&pilaUnion,&pilaB);
     }
     pasarTodo(&pilaB,&pilaUnion);
 
+    ///Aca tengo anidado dos ciclos while
+    ///Tengo que quedarme con todos los elementos que esten en ambas pilas, pero sin repetirlos
     while (!pilavacia(&auxiliarA)){
+        ///Mientras B no este vacia...
         while (!pilavacia(&auxiliarB)){
+            ///SI el tope de A es igual al tope de B
             if (tope(&auxiliarA) == tope(&auxiliarB)){
+                ///Me deshago del tope de B, porque esta repetido y no me sirve, lo desapilo
                 desapilar(&auxiliarB);
             } else {
+                ///Si no... paso el tope de B a la pila "diferentes"
                 pasar(&diferentes,&auxiliarB);
             }
         }
+        ///Devuelvo los diferentes a la pila "auxiliarB"
         pasarTodo(&auxiliarB,&diferentes);
+        ///Paso el tope de A a la pila union y vuelvo a comparar los elementos de B con el nuevo tope de A
         pasar(&pilaUnion,&auxiliarA);
     }
+    ///Ahora la pila union tiene todos los elementos de A, y le paso los elementos de B que no estaban ya en A
     pasarTodo(&pilaUnion,&auxiliarB);
 
     mostrar(&pilaA);
